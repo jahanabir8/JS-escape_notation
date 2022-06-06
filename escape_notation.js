b2275dcdcf16f8761f9dff8274613d1f67eccc69
jahanabir8 <abir.opediatech@gmail.com>
2022-06-06 12:06:37 +0600

    first-upload

---
+++ escape_notation.js
@@ -0,0 +1,31 @@
+
+var str = 'This is a String'
+console.log(str)
+
+var str1 = 'This is a "String"'
+var str2 = "This is a 'String'"
+console.log(str1)
+console.log(str2)
+
+
+var str1 = 'This is \'String\''
+var str2 = 'This is \"String\"'
+console.log(str1)
+console.log(str2)
+
+
+// for new line
+var str = 'This is \n String'
+console.log(str)
+
+
+// for tab
+var str = 'This is \t String'
+console.log(str)
+
+// for back-slash and forward-slash
+var str = 'This is \\String/'
+console.log(str)
+
+var str = 'This is \r String'
+console.log(str)
